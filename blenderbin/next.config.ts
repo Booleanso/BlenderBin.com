1a5d48539c8655bd640e56a98d6225aa98a20d02
gitkevinvera <ceo@webrend.com>
2024-12-01 13:42:38 -0500

    added firebase config and stripe payemnts

--- blenderbin/next.config.ts
+++ blenderbin/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Add a rule to ignore all files in the specified folders
+    config.module.rules.push({
+      test: /roadmap|settings|installation|components|aws-s3\//, // Match any file in folder1, folder2, or folder3
+      use: "ignore-loader", // Use ignore-loader to prevent processing
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
