48a45272160f9d6de5aaf07c190ce02e40bdf8dc
gitkevinvera <ceo@webrend.com>
2024-12-04 19:26:19 -0500

    added css and spliend esignf or main page

--- blenderbin/app/page.tsx
+++ blenderbin/app/page.tsx
@@ -1,101 +1,51 @@
-import Image from "next/image";
+// import Image from "next/image";
+import "./css/main-page/hero-section.css";
+import "./css/main-page/about-section.css";
+import "./css/main-page/blender-screen.css";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="body-main">
+      <section className="hero-section"> 
+        <iframe
+          className="spline"
+          src="https://my.spline.design/blenderbincopy-1749f0bebc4ffdbe4c9fb47c98860979/"
+          width="100%"
+          height="100%"
+          style={{ border: 'none' }}
+          allowFullScreen
+        ></iframe>
+        <div className="hero-desc">
+          <div className="new-tag">SPANKING NEW</div>
+          <h1 className="hero-title">All your Blender addons under one subscription.</h1>
+          <p>You thought Gojo saw infinity? Wait till you see this.</p>
+          <div className="hero-buttons">
+            <button className="trial-button">Start Your Free Trial</button>
+            <button className="explore-button">Explore Add-Ons</button>    
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+
+      <section className="blender-screen">
+        <div className="screen-container">
+          <div className="actual-screne">
+
+          </div>
+        </div>
+      </section>
+
+
+      <section className="quick-about">
+        {/* <Image>
+
+        </Image> */}
+        <h1 className="about-title">Designed to fix annoyances.</h1>
+        <p className="about-paragraph">The creators for BlenderBin thought of nan idea, to bring together an addon library that focuses on easy addons that everyone needs, but no one wants to pay for individually. So, we introduced a subscription model.</p>
+        <button className="see-subscriptions">See Subscriptions</button>  
+      </section>
+
+
     </div>
   );
 }
