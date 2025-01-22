054c5cc21a805ce19bbf8078b8931379c3d104f7
gitkevinvera <ceo@webrend.com>
2025-01-22 07:13:14 -0500

    not much

--- blenderbin/app/page.tsx
+++ blenderbin/app/page.tsx
@@ -7,19 +7,19 @@ import "./css/main-page/about-section.css";
 import "./css/main-page/blender-screen.css";
 import "./css/main-page/scroll-addons.css";
 
-import HeroSection from './components/main-page/HeroSection';
-import TabComponent from './components/main-page/TabComponent';
-import FeaturesSection from './components/main-page/FeatureSection';
-import Newsletter from './components/main-page/Newsletter';
-import LovedBy from './components/main-page/LovedBy';
-import BentoBox from './components/main-page/BentoBox';
+import HeroSection from '../app/components/main-page/HeroSection';
+import TabComponent from '../app/components/main-page/TabComponent';
+import FeaturesSection from '../app/components/main-page/FeatureSection';
+import Newsletter from '../app/components/main-page/Newsletter';
+import LovedBy from '../app/components/main-page/LovedBy';
+import BentoBox from '../app/components/main-page/BentoBox';
 import "./css/main-page/bento-box.css";
-import ConvergingFeatures from './components/main-page/ConvergingFeatures';
+import ConvergingFeatures from '../app/components/main-page/ConvergingFeatures';
 
 
 
-import DTFA from "./components/main-page/DTFA";
-import VideoSection from "./components/main-page/VideoSection";
+import DTFA from "../app/components/main-page/DTFA";
+import VideoSection from "../app/components/main-page/VideoSection";
 
 export default function Home() {
   const [scrollY, setScrollY] = useState(0);
@@ -48,8 +48,6 @@ export default function Home() {
       <TabComponent />
 
       <FeaturesSection />
-
-
 
       <BentoBox />
 
